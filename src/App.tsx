db1ded516f1b18820fa5a6d872e2305808de783d
Aris Charalampidis <aris_charalampides@hotmail.com>
2025-10-16 23:12:54 +0300

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import Layout from "./components/layout/Layout.tsx";
 import Todo from "./components/todo/Todo.tsx";
 
-
 function App() {
 
-
-  return (
-    <>
-        <Layout>
-            <Todo/>
-
-        </Layout>
-
-    </>
-  )
+    return (
+        <>
+            <Layout>
+                <Todo/>
+            </Layout>
+        </>
+    )
 }
 
 export default App
